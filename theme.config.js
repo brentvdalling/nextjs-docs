cf7548e1375c71a7568e93957202443d70c554f5
Brent Dalling <brent.dalling@southernutahdesign.com>
2021-02-17 16:56:33 -0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/bvdalling/TimelyCMS-Documentation',
+  titleSuffix: ' – Timely Systems',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next Site Builder
+        Code Docs
       </span>
     </>
   ),
@@ -37,5 +37,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>
+  footerText: <>MIT {new Date().getFullYear()} © Timely Systems.</>
 }
